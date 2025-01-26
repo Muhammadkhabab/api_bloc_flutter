c2beca2a823b69437d66ad314fabb633f290aee4
Query Byte Tech <khababkhattak332@gmail.com>
2025-01-27 00:12:49 +0500

    showing the data from the api

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,22 +1,56 @@
+import 'package:bloc_api_project/bloc/post_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomePostScreen extends StatefulWidget {
+  const HomePostScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<HomePostScreen> createState() => _HomePostScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomePostScreenState extends State<HomePostScreen> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<PostBloc>().add(PostFetched());
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Post Bloc Api'),
-        centerTitle: true,
+        title: Text('Post List using bloc'),
       ),
-      body: Column(
-        children: [],
+      body: BlocBuilder<PostBloc, PostState>(
+        builder: (context, state) {
+          if (state.postState == PostStatus.initial) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (state.postState == PostStatus.success) {
+            return ListView.builder(
+              itemCount: state.posts.length,
+              itemBuilder: (context, index) {
+                final post = state.posts[index];
+                return ListTile(
+                  leading: Text(post.id.toString()),
+                  title: Text(post.title.toString()),
+                  subtitle: Text(post.title.toString()),
+                );
+              },
+            );
+          } else {
+            return Center(
+              child: Text(state.message),
+            );
+          }
+        },
       ),
     );
   }
